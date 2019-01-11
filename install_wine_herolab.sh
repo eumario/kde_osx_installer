b34ab4f33350bbc6e995fc03c5e7c0b4df56f739
Mario Steele <mario@ruby-im.net>
2019-01-11 00:11:31 -0600

    Added creation of fonts folder in /usr/local/share

--- install_wine_herolab.sh
+++ install_wine_herolab.sh
@@ -5,6 +5,7 @@ sudo pacman -S --noconfirm wine winetricks lib32-gnutls
 
 zenity --info --no-wrap --text="Press OK to start downloading Windows Fonts..."
 wget https://www.w7df.com/files0/Windows7DefaultFonts.zip?dl=1 -O Windows7DefaultFonts.zip
+mkdir -p /usr/local/share/fonts/
 unzip Windows7DefaultFonts.zip
 sudo cp -R Windows7DefaultFonts/* /usr/local/share/fonts/
 sudo fc-cache -vf
